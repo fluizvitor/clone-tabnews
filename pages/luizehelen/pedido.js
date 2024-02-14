c058b58041ef9dd575f804e2460c6d89a15566ec
fluizvitor <63168726+fluizvitor@users.noreply.github.com>
2024-02-14 05:12:20 +0000

    e

--- pages/luizehelen/pedido.js
+++ pages/luizehelen/pedido.js
@@ -4,19 +4,35 @@ const Index = () => {
   const [buttonNoStyle, setButtonNoStyle] = useState({});
 
   const handleNoButtonClick = () => {
-    const randomPosition = () => Math.floor(Math.random() * 10) - 5;
+    const randomPosition = () => Math.random() * (window.innerWidth - 100);
     setButtonNoStyle({
       position: "absolute",
-      top: `${50 + randomPosition()}vh`,
-      left: `${50 + randomPosition()}vw`,
+      top: `${Math.random() * (window.innerHeight - 100)}px`,
+      left: `${randomPosition()}px`,
     });
+  };
+
+  const handleYesButtonClick = () => {
+    alert("Você fez a melhor escolha da sua vida!!!");
+    window.location.href = "https://youtu.be/vSsUDOpzYOs?si=ADcttkxOsTf17JAs";
   };
 
   return (
     <div style={{ textAlign: "center", marginTop: "200px" }}>
-      <h1>Helen, quer namorar comigo?</h1>
+      <style jsx global>{`
+        html,
+        body {
+          height: 100%;
+          margin: 0;
+          padding: 0;
+          background-color: #ffc0cb;
+        }
+      `}</style>
+      <h1>Helen, você aceita namorar comigo?</h1>
       <div style={{ marginTop: "20px" }}>
-        <button style={{ marginRight: "10px" }}>Sim</button>
+        <button style={{ marginRight: "10px" }} onClick={handleYesButtonClick}>
+          Sim
+        </button>
         <button onClick={handleNoButtonClick} style={buttonNoStyle}>
           Não
         </button>
